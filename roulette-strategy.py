6c7e302c591209284182674097cea3635d0749b4
salimahi <76531234+salimahi@users.noreply.github.com>
2023-07-01 19:08:00 -0400

    Create roulette-strategy.py
    A python code to test out a roulette strategy where the player continuously chooses black and adds $5 to the bet until they either double their initial cash or lose everything.

---
+++ roulette-strategy.py
@@ -0,0 +1,62 @@
+import random
+
+#Cash on Hand
+totalCash = 100
+doubleCash = 2*totalCash
+
+#Start at Zero
+bet = 0
+
+#Setting up Tracking the Bet and Wheel Results
+betList = []
+wheelResults = []
+
+#Either losing everything or doubling the money
+while totalCash > 0 and totalCash < doubleCash:
+        #Adding $5 each bet
+        bet = bet + 5
+        #Tracking the bet
+        betList.append(bet)
+
+        #Spinnging the roulette wheel
+        wheel = random.randint(0,37)
+
+        #Zero or Double Zero  Result
+        if wheel == 0 or wheel == 37:
+                wheelResults.append('zero')
+                #Checking if you still have cash to bet with
+                if totalCash >= bet:
+                        totalCash = totalCash - bet
+                elif totalCash < bet:
+                        totalCash == 0
+                
+
+        #Landing on Red
+        elif (wheel % 2) != 0:
+                wheelResults.append('red')
+                #Checking if you still have cash to bet with
+                if totalCash >= bet:
+                        totalCash = totalCash - bet
+                elif totalCash < bet:
+                        totalCash == 0
+                
+        #Landing on Black
+        elif (wheel % 2) == 0:
+                totalCash = totalCash + bet
+                wheelResults.append('black')
+
+num_of_zeros = wheelResults.count('zero')
+num_of_red = wheelResults.count('red')
+num_of_black = wheelResults.count('black')
+
+print(f"""You finished the game with a bet of {betList[-1]} and the result was {wheelResults[-1]}.
+You are going home with ${totalCash}
+Within the game, you hit:
+0 or 00 {num_of_zeros} times
+red {num_of_red} times
+black {num_of_black} times.
+""")
+
+    
+
+    
